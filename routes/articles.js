9accfb7178a0b7cd0b6d9ac29a2ce805baed8243
Sahar Moshe <saharm123456@gmail.com>
2022-11-10 13:22:33 +0200

    added routes for get,post and delete user article

--- routes/articles.js
+++ routes/articles.js
@@ -1,5 +1,18 @@
 const router = require('express').Router();
 
+const getUserSavedArticles = () => {
 
+};
+const createArticle = () => {
+
+};
+
+const deleteUserArticle = () => {
+
+};
+
+router.get('/articles', getUserSavedArticles);
+router.post('/articles', createArticle);
+router.delete('/articles/:articleId', deleteUserArticle);
 
 module.exports = router;

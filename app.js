dd9f498958a3067b67670fd80ca183ea5a073e7c
Sahar Moshe <saharm123456@gmail.com>
2022-11-10 13:33:04 +0200

    add bodyParser & helmet

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
-const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
 const router = require('./routes/router');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(helmet());
 app.use(router);
 const { PORT = 3000 } = process.env;
 

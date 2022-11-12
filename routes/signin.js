6049dc8bc0876e6ac696eb4eb38a2b364bd752e3
Sahar Moshe <saharm123456@gmail.com>
2022-11-12 17:38:21 +0200

    fix login path

--- routes/signin.js
+++ routes/signin.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
+const { login } = require('../controllers/users');
 
-const login = () => {
-
-};
 router.post('/signin', login);
 
 module.exports = router;

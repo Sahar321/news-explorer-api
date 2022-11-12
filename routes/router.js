0c28c10862719fcafa5018fdbb08c9bcfcec5f34
Sahar Moshe <saharm123456@gmail.com>
2022-11-12 17:37:36 +0200

    adding authorization for user routes

--- routes/router.js
+++ routes/router.js
@@ -3,10 +3,11 @@ const users = require('./users');
 const articles = require('./articles');
 const signin = require('./signin');
 const signup = require('./signup');
+const authorized = require('../middleware/auth');
 
 router.all('/signin*', signin);
 router.all('/signup*', signup);
-router.all('/users*', users);
+router.all('/users*', authorized, users);
 router.all('/articles*', articles);
 
 router.all('*', (req, res, next) => {

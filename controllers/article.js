3f79cf618ed460ffe992ad8af6ddd4b0a1c84fe4
Sahar Moshe <saharm123456@gmail.com>
2022-11-12 21:19:00 +0200

    implement article controller(get Articles, create new and delete)

---
+++ controllers/article.js
@@ -0,0 +1,60 @@
+/*  eslint no-underscore-dangle: ['error', { 'allow': ['_id'] }] */
+const { ObjectId } = require('mongodb');
+const Article = require('../models/article');
+const NotFoundError = require('../middleware/errors/NotFoundError');
+const ForbiddenError = require('../middleware/errors/ForbiddenError');
+
+const getAllArticles = (req, res, next) => {
+  Article.find({})
+    .orFail(() => {
+      throw new NotFoundError('Not found Articles');
+    })
+    .then((Articles) => {
+      res.send(Articles);
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+const createNewArticle = (req, res, next) => {
+  const {
+    keyword, title, text, date, source, link, image,
+  } = req.body;
+
+  const owner = req.user._id;
+
+  Article.create({
+    keyword,
+    title,
+    text,
+    date,
+    source,
+    link,
+    image,
+    owner,
+  })
+    .then((article) => res.send(article))
+    .catch((err) => next(err));
+};
+const deleteArticleById = (req, res, next) => {
+  Article.findOneAndRemove({
+    _id: ObjectId(req.params.articleId),
+    owner: ObjectId(req.user._id),
+  })
+    .orFail(() => {
+      throw new ForbiddenError('You do not have permissions to delete this Article');
+    })
+    .then((article) => {
+      res.send(article);
+    })
+    .catch(() => {
+      next(new ForbiddenError('You do not have permissions to delete this Article'));
+    });
+};
+
+module.exports = {
+  getAllArticles,
+  createNewArticle,
+  deleteArticleById,
+};

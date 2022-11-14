eef2904a56737302f8a2a055f3f69f8aba9da7d3
Sahar Moshe <saharm123456@gmail.com>
2022-11-14 04:30:20 +0200

    make default jwt for dev environment

--- constant/constant.js
+++ constant/constant.js
@@ -1,2 +1,2 @@
-const defaultJwtSecret = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
-module.exports = defaultJwtSecret;
+const developmentJwtSecret = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+module.exports = developmentJwtSecret;

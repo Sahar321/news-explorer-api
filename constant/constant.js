9eac4948b16887f3a279bf8362e061806d2d4eeb
Sahar Moshe <saharm123456@gmail.com>
2022-11-14 05:26:47 +0200

    move limiter to constant folder

--- constant/constant.js
+++ constant/constant.js
@@ -1,2 +1,16 @@
+const rateLimit = require('express-rate-limit');
+const TooManyRequestsError = require('../middleware/errors/TooManyRequestsError');
+
+const limiter = rateLimit({
+  // max 100 request per 10 minutes
+  windowMs: 10 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: () => {
+    throw new TooManyRequestsError();
+  },
+});
 const developmentJwtSecret = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
-module.exports = developmentJwtSecret;
+
+module.exports = { developmentJwtSecret, limiter };

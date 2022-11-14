ec3e97b5fd837d724b49a90f3792160d0edbd283
Sahar Moshe <saharm123456@gmail.com>
2022-11-14 03:55:50 +0200

    fix signup ValidationSchema; name most in length between 2 to 30

--- routes/signup.js
+++ routes/signup.js
@@ -8,7 +8,7 @@ const ValidationSchema = {
       .keys({
         email: Joi.string().required().email(),
         password: Joi.string().required(),
-        name: Joi.string().required(),
+        name: Joi.string().required().min(2).max(30),
       })
       .unknown(true),
   }),

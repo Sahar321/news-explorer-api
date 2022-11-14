dc34225f3174ca76ef013b3d6102efe84290facb
Sahar Moshe <saharm123456@gmail.com>
2022-11-14 03:13:41 +0200

    fix catch(trow error on invalid token)

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,16 @@
-/* eslint-disable */
-const jwt = require("jsonwebtoken");
-const UnauthorizedError = require("./errors/UnauthorizedError");
-const defaultJwtSecret = require("../constant/constant");
-module.exports = authorized = (req, res, next) => {
+const jwt = require('jsonwebtoken');
+const UnauthorizedError = require('./errors/UnauthorizedError');
+const InValidRequestError = require('./errors/InValidRequestError');
+const defaultJwtSecret = require('../constant/constant');
 
+const authorized = (req, res, next) => {
   const { JWT_SECRET = defaultJwtSecret } = process.env;
   const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith("Bearer ")) {
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new UnauthorizedError();
   }
 
-  const token = authorization.replace("Bearer ", "");
+  const token = authorization.replace('Bearer ', '');
 
   // if token is verified, save the payload
   let payload;
@@ -18,7 +18,7 @@ module.exports = authorized = (req, res, next) => {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     // otherwise, return an error
-    //throw new InValidRequestError();
+    throw new InValidRequestError('invalid token');
   }
 
   /* Save payload to request. This makes the payload available
@@ -29,3 +29,5 @@ module.exports = authorized = (req, res, next) => {
   // sending the request to the next middleware
   next();
 };
+
+module.exports = authorized;

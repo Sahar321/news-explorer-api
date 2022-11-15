9d5283ee3771f4985cfe9a54b58f95aaef5d1331
Sahar Moshe <saharm123456@gmail.com>
2022-11-15 21:00:47 +0200

    remove developmentJwtSecret and move it to config file

--- constant/constant.js
+++ constant/constant.js
@@ -1,5 +1,5 @@
 const rateLimit = require('express-rate-limit');
-const TooManyRequestsError = require('../middleware/errors/TooManyRequestsError');
+const TooManyRequestsError = require('./errors/TooManyRequestsError');
 
 const limiter = rateLimit({
   // max 100 request per 10 minutes
@@ -11,6 +11,5 @@ const limiter = rateLimit({
     throw new TooManyRequestsError();
   },
 });
-const developmentJwtSecret = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
 
-module.exports = { developmentJwtSecret, limiter };
+module.exports = { limiter };

bfb73213b96e31df47d2341998447224e6e6665c
Sahar Moshe <saharm123456@gmail.com>
2022-11-15 21:04:20 +0200

    move handleMainError to separate file

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
-const { errors, isCelebrateError } = require('celebrate');
+const { errors } = require('celebrate');
 const cors = require('cors');
 const { limiter } = require('./constant/constant');
+const handleMainError = require('./constant/errors/handleMainError');
+const { developmentDatabaseAddress } = require('./constant/config')
 // app
 const router = require('./routes/router');
 
@@ -15,21 +17,7 @@ const { requestLogger, errorLogger } = require('./middleware/logger');
 const app = express();
 const { PORT = 3000 } = process.env;
 // if .env not exist(=development mode) use default database address.
-const { DATABASE_ADDRESS = 'mongodb://localhost:27017/newsExplorer' } = process.env;
-
-const handleMainError = (err, req, res, next) => {
-  let { statusCode = 500 } = err;
-  const { message } = err;
-  // change status code if is CelebrateError, otherwise
-  // it will send the default error message
-  if (isCelebrateError(err)) {
-    statusCode = 400;
-  }
-
-  res.status(statusCode).send({
-    message: statusCode === 500 ? 'An error occurred on the server' : message,
-  });
-};
+const { DATABASE_ADDRESS = developmentDatabaseAddress } = process.env;
 
 app.use(requestLogger);
 app.use(bodyParser.urlencoded({ extended: true }));

e2959a504ec5e48157e06afa585147d2a31376b4
Sahar Moshe <saharm123456@gmail.com>
2022-11-15 21:01:37 +0200

    default jwt & db address for Dev mode

---
+++ constant/config.js
@@ -0,0 +1,4 @@
+const developmentJwtSecret = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+const developmentDatabaseAddress = 'mongodb://localhost:27017/newsExplorer';
+
+module.exports = { developmentJwtSecret, developmentDatabaseAddress };

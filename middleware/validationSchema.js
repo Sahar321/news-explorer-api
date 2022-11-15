35b7b97f0ac6f6a3860abface9edafb402a9a604
Sahar Moshe <saharm123456@gmail.com>
2022-11-15 22:22:15 +0200

    remove .unknown(true),

--- middleware/validationSchema.js
+++ middleware/validationSchema.js
@@ -3,42 +3,34 @@ const { celebrate, Joi } = require('celebrate');
 const ValidationSchema = {
   article: {
     createNewArticle: celebrate({
-      body: Joi.object()
-        .keys({
-          keyword: Joi.string().required(),
-          title: Joi.string().required(),
-          text: Joi.string().required(),
-          date: Joi.string().required(),
-          source: Joi.string().required(),
-          link: Joi.string().required().uri(),
-          image: Joi.string().required().uri(),
-        })
-        .unknown(true),
+      body: Joi.object().keys({
+        keyword: Joi.string().required(),
+        title: Joi.string().required(),
+        text: Joi.string().required(),
+        date: Joi.string().required(),
+        source: Joi.string().required(),
+        link: Joi.string().required().uri(),
+        image: Joi.string().required().uri(),
+      }),
     }),
     articleId: celebrate({
-      params: Joi.object()
-        .keys({
-          articleId: Joi.string().hex().required(),
-        })
-        .unknown(true),
+      params: Joi.object().keys({
+        articleId: Joi.string().hex().required(),
+      }),
     }),
   },
   signin: celebrate({
-    body: Joi.object()
-      .keys({
-        email: Joi.string().required().email(),
-        password: Joi.string().required(),
-      })
-      .unknown(true),
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
   }),
   signup: celebrate({
-    body: Joi.object()
-      .keys({
-        email: Joi.string().required().email(),
-        password: Joi.string().required(),
-        name: Joi.string().required().min(2).max(30),
-      })
-      .unknown(true),
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+      name: Joi.string().required().min(2).max(30),
+    }),
   }),
 };
 

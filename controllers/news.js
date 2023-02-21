cfb70ebe0da8db988b5fd530ac52ccd8d888277d
Sahar Moshe <saharm123456@gmail.com>
2023-02-21 03:37:05 +0200

    reactions

--- controllers/news.js
+++ controllers/news.js
@@ -1,13 +1,35 @@
+/* eslint-disable*/
 const fetch = require('cross-fetch');
-
-const fetchNewsArticles = (req, res, next) => {
+const Reaction = require('../models/reaction');
+const fetchNewsArticles = async (req, res, next) => {
+  const newArray = [];
+  let reactions;
+  try {
+    reactions = await Reaction.find({});
+  } catch (err) {
+    next(err);
+  }
   const { q, pageSize } = req.query;
   const apiKey = '5dac586067ad4ee7b2797a379e910521'; // todo: process.env.NEWS_API_KEY;
   const baseUrl = 'https://newsapi.org/v2/everything';
-  fetch(`${baseUrl}?q=${q}&apiKey=${apiKey}&pageSize=${pageSize}&sortBy=popularity`)
+  let times = 0;
+  fetch(
+    `${baseUrl}?q=${q}&apiKey=${apiKey}&pageSize=${pageSize}&sortBy=popularity`
+  )
     .then((response) => response.json())
     .then((data) => {
-      res.send(data);
+      data.articles.map((article) => {
+        const reaction = reactions.find((item) => item.link === article.url);
+        reaction
+          ? newArray.push({ ...article, reactionId: reaction.reactionId })
+          : newArray.push(article);
+      });
+
+      res.send({
+        articles: newArray,
+        status: 'ok',
+        totalResults: data.totalResults,
+      });
     })
     .catch((err) => {
       next(err);

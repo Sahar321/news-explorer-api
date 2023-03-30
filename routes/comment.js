eb3878a5d68d3cc4a15cd3e7f62565da763f3622
Sahar Moshe <saharm123456@gmail.com>
2023-03-30 03:58:13 +0300

    add thank-you path -post

--- routes/comment.js
+++ routes/comment.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const { saveComment } = require('../controllers/comment');
+const { thankYou } = require('../controllers/thankYou');
 
 router.post('/comment', saveComment);
+router.post('/comment/thank-you', thankYou);
 
 module.exports = router;

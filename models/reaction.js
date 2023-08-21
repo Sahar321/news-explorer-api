c65f560d4f6984f0f41afe45f6a53d21e7081621
Sahar <saharm123456@gmail.com>
2023-08-22 00:44:03 +0300

    add aggregate getArticleReactionSummary

--- models/reaction.js
+++ models/reaction.js
@@ -1,27 +1,86 @@
+/*eslint-disable */
 const mongoose = require('mongoose');
 
-const reactionSchema = mongoose.Schema(
-  {
-    type: {
-      type: String,
-      enum: ['LOL', 'WOW', 'LIKE', 'SAD', 'LOVE'],
-      required: true,
-    },
-    date: {
-      type: String,
-    },
-    link: {
-      type: String,
-      required: true,
-      index: true,
-    },
-    owner: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'user',
-      required: true,
-    },
+const reactionSchema = mongoose.Schema({
+  type: {
+    type: String,
+    enum: ['LOL', 'WOW', 'LIKE', 'SAD', 'LOVE'],
+    required: true,
   },
+  date: {
+    type: String,
+  },
+  link: {
+    type: String,
+    required: true,
+    index: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+});
 
-);
+reactionSchema.statics.getArticleReactionSummary = async function (
+  linkId,
+  ownerId
+) {
+  const ownerIdAsObjectId = mongoose.Types.ObjectId(ownerId);
+
+  const result = await this.aggregate([
+    {
+      $match: {
+        link: linkId,
+      },
+    },
+    {
+      $facet: {
+        reactionCountByType: [
+          {
+            $group: {
+              _id: '$type',
+              count: { $sum: 1 },
+            },
+          },
+          {
+            $group: {
+              _id: null,
+              countByType: {
+                $push: {
+                  k: '$_id',
+                  v: '$count',
+                },
+              },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              countByType: {
+                $arrayToObject: '$countByType',
+              },
+            },
+          },
+        ],
+        selectedOwnerReaction: [
+          {
+            $match: {
+              owner: ownerIdAsObjectId,
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  // Extract data from the result.
+  const reactionsInfo = {
+    countByType: result[0]?.reactionCountByType[0]?.countByType || {},
+    ownerReactionType: result[0]?.selectedOwnerReaction[0]?.type || null,
+  };
+
+  return reactionsInfo;
+};
 
 module.exports = mongoose.model('reaction', reactionSchema);

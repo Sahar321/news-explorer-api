b251b0336d3f6875101aca2ed4a9d620c8b6a465
Sahar Moshe <saharm123456@gmail.com>
2023-08-28 02:50:19 +0300

    add comments route

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const { updateProfile } = require('../controllers/users');
+const { getAllUserComments } = require('../controllers/comment');
 const { profile } = require('../middleware/validationSchema');
 
 router.post('/profile', profile, updateProfile);
+router.get('/profile/comments', getAllUserComments);
 module.exports = router;

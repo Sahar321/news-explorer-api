68e05742f3674fb60b1573b5f52ddbfde1e89756
Sahar Moshe <saharm123456@gmail.com>
2023-08-28 02:49:49 +0300

    new func getAllUserComments

--- controllers/comment.js
+++ controllers/comment.js
@@ -4,7 +4,7 @@ const ForbiddenError = require('../constant/errors/ForbiddenError');
 const Comment = require('../models/comment');
 const Article = require('../models/article');
 const Users = require('../models/user');
-const {getAllArticleComments} = require('../controllers/article');
+const { getAllArticleComments } = require('../controllers/article');
 const saveComment = (req, res, next) => {
   const { link, text } = req.body;
 
@@ -21,6 +21,22 @@ const saveComment = (req, res, next) => {
     .catch((err) => next(err));
 };
 
+const getAllUserComments = (req, res, next) => {
+  const owner = req.user._id;
+
+  Comment.find({
+    owner,
+  })
+    .lean()
+    .then((userComments) => {
+      res.send(userComments);
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
+};
 module.exports = {
   saveComment,
+  getAllUserComments,
 };

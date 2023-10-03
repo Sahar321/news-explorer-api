ba1474929446ea25798f91a46600a332aae5f2f9
Sahar Moshe <saharm123456@gmail.com>
2023-10-03 14:01:18 +0300

    add  this.shouldDisplayToUser

--- constant/errors/UnauthorizedError.js
+++ constant/errors/UnauthorizedError.js
@@ -1,7 +1,8 @@
 class UnauthorizedError extends Error {
-  constructor(message) {
+  constructor(message, shouldDisplayToUser = false) {
     super(message || 'You are not authorized to access');
     this.statusCode = 401;
+    this.shouldDisplayToUser = shouldDisplayToUser;
     this.message = message || 'You are not authorized to access';
   }
 }

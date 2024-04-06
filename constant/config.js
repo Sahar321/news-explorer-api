ee7ea2a3a382af0be3767d42142797f55d99ec24
Sahar321 <Saharm123456@gmail.com>
2024-04-07 01:04:27 +0300

    Update config.js

--- constant/config.js
+++ constant/config.js
@@ -1,4 +1,3 @@
-const developmentJwtSecret = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
-const developmentDatabaseAddress = 'mongodb://localhost:27017/newsExplorer';
+ const developmentDatabaseAddress = 'mongodb://localhost:27017/newsExplorer';
 
 module.exports = { developmentJwtSecret, developmentDatabaseAddress };
